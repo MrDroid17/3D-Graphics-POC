3111b7f8ed09155b469adae34710b2c1dbb36edf
Sobhit Kumar <sobhit.kumar@claritusconsulting.com>
2019-10-16 14:48:26 +0530

    Importing blender file

---
+++ gltf.js
@@ -0,0 +1,92 @@
+{/* <script> */ }
+let box_status = false;
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color('skyblue');
+// scene.overrideMaterial = new THREE.MeshLambertMaterial();
+
+let renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.gammaOutput = true;
+renderer.gammaFactor = 2.2;
+renderer.autoClear = false;
+document.body.appendChild(renderer.domElement);
+
+// add listener to resize
+window.addEventListener('resize', () => {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+})
+
+
+// controls
+control = new THREE.OrbitControls(camera, renderer.domElement);
+const LENGTH = 10;
+const HEIGHT = 3;
+const WIDTH = 8;
+const THICKNESS = 0.05;
+
+// camera position
+camera.position.z = 50;
+
+var loader = new THREE.GLTFLoader();
+loader.load(
+   './3d-asset/testanim03.glb',
+   function ( gltf ) {
+      var scale = 5.6;
+ 
+      scene.add( gltf.scene )
+   },
+);
+
+// right side
+// let rsside = new THREE.Object3D();
+// generateRightSide(THICKNESS, HEIGHT, WIDTH, rsside);
+// scene.add(rsside);
+
+// top side
+// let tsside = new THREE.Object3D();
+// generateTopSide(LENGTH, THICKNESS, WIDTH, rsside);
+// scene.add(tsside);
+
+// bottom
+// let bt_geometry = new THREE.BoxBufferGeometry(LENGTH, THICKNESS, WIDTH);
+// let bt_material = new THREE.MeshBasicMaterial({ color: 0x0ff0ff, side: THREE.DoubleSide });
+// let bottom = new THREE.Mesh(bt_geometry, bt_material);
+// bottom.position.set(0, -1.5, 0);
+// scene.add(bottom);
+
+function generateRightSide(th, ht, wt, rside){
+  let rs_geometry = new THREE.BoxBufferGeometry(th, ht, wt);
+  let rs_material = new THREE.MeshBasicMaterial({ color: 0x0bcdff, side: THREE.DoubleSide });
+  let right_side = new THREE.Mesh(rs_geometry, rs_material);
+  right_side.position.set(5, 0, 0);
+  right_side.position.set(5, -1.5, 0);
+  rside.add(right_side);
+}
+
+// top
+function generateTopSide(lt, th, wt, tside){
+let ts_geometry = new THREE.BoxBufferGeometry(lt, th, wt);
+let ts_material = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide });
+let top_side = new THREE.Mesh(ts_geometry, ts_material);
+top_side.position.set(0, 1.5, 0);
+tside.add(top_side);
+}
+
+// run game loop(update, render, repeat) to animate
+let GameLoop = () => {
+  requestAnimationFrame(GameLoop);
+  render();
+}
+// Draw Scene
+let render = () => {
+  renderer.render(scene, camera);
+}
+
+//update();
+GameLoop()
+// </script>
---
+++ main4.js
@@ -0,0 +1,275 @@
+{/* <script> */ }
+let box_status = false;
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color('skyblue');
+// scene.overrideMaterial = new THREE.MeshLambertMaterial();
+
+let renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.autoClear = false;
+document.body.appendChild(renderer.domElement);
+
+// add listener to resize
+window.addEventListener('resize', () => {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+})
+
+
+// controls
+control = new THREE.OrbitControls(camera, renderer.domElement);
+const LENGTH = 10;
+const HEIGHT = 3;
+const WIDTH = 8;
+const THICKNESS = 0.05;
+
+
+// bottom
+let bt_geometry = new THREE.BoxBufferGeometry(LENGTH, THICKNESS, WIDTH);
+let bt_material = new THREE.MeshBasicMaterial({ color: 0x0ff0ff, side: THREE.DoubleSide });
+let bottom = new THREE.Mesh(bt_geometry, bt_material);
+bottom.position.set(0, -1.5, 0);
+scene.add(bottom);
+camera.position.z = 10;
+
+// left side
+let ls_geometry = new THREE.BoxBufferGeometry(THICKNESS, HEIGHT, WIDTH);
+let ls_material = new THREE.MeshBasicMaterial({ color: 0x0fadff, side: THREE.DoubleSide });
+let left_side = new THREE.Mesh(ls_geometry, ls_material);
+left_side.position.set(-5, 0, 0);
+scene.add(left_side);
+
+// right side
+let rs_geometry = new THREE.BoxBufferGeometry(THICKNESS, HEIGHT, WIDTH);
+let rs_material = new THREE.MeshBasicMaterial({ color: 0x0bcdff, side: THREE.DoubleSide });
+let right_side = new THREE.Mesh(rs_geometry, rs_material);
+right_side.position.set(5, 0, 0);
+scene.add(right_side);
+// front side
+let fs_geometry = new THREE.BoxBufferGeometry(LENGTH, HEIGHT, THICKNESS);
+let fs_material = new THREE.MeshBasicMaterial({ color: 0xabcdff, side: THREE.DoubleSide });
+let front_side = new THREE.Mesh(fs_geometry, fs_material);
+front_side.position.set(0, 0, 4);
+scene.add(front_side);
+// back side
+let bs_geometry = new THREE.BoxBufferGeometry(LENGTH, HEIGHT, THICKNESS);
+let bs_material = new THREE.MeshBasicMaterial({ color: 0xcdefff, side: THREE.DoubleSide });
+let back_side = new THREE.Mesh(bs_geometry, bs_material);
+back_side.position.set(0, 0, -4);
+scene.add(back_side);
+
+
+// top
+let ts_geometry = new THREE.BoxBufferGeometry(LENGTH, THICKNESS, WIDTH);
+let ts_material = new THREE.MeshBasicMaterial({ color: 0x0000ff, side: THREE.DoubleSide });
+let top_side = new THREE.Mesh(ts_geometry, ts_material);
+top_side.position.set(0, 1.5, 0);
+scene.add(top_side);
+// let top_side = new THREE.Mesh(geometry2, material2);
+// top_side.position.set(0, 1.5, 0);
+// scene.add(top_side);
+// base.rotation.x = Math.PI/2;
+let x_axis, y_axis, z_axis;
+const open_x_val = 0.7;
+const open_y_val = 4;
+const open_z_val = 0;
+const open_rot_z_val = -Math.PI / 6;
+
+const close_x_val = 0;
+const close_y_val = 1.5;
+const close_z_val = 0;
+const close_rot_z_val = 0;
+let rotZ = 0;
+let rotY = close_y_val;
+let rotX = close_x_val;
+xDisplacement = -5;
+yDisplacement = 0;
+zDisplacementPositive = 4;
+zDisplacementNegative = -4;
+
+let intervalObj;
+boxControl = () => {
+
+  if (!box_status) {
+    box_status = true;
+    openRightSide();
+    intervalObj = setInterval(() => {
+      openBox();
+    }, 50);
+  } else {
+    box_status = false;
+    intervalObj = setInterval(() => {
+      closeBox();
+    }, 50);
+  }
+}
+
+openRightSide = () => {
+
+}
+
+
+// game logic
+// let update = () => {    
+// if(box_status){
+//   openBox()
+// }
+
+// };
+openBox = () => {
+  if (rotZ >= open_rot_z_val) {
+    rotZ -= 0.01;
+    rotY += 0.048;
+    xDisplacement -= 0.026;
+    yDisplacement -= 0.018;
+    zDisplacementNegative -= 0.024;
+    zDisplacementPositive += 0.024;
+    
+    (rotZ >= open_rot_z_val / 2) ? rotX += 0.005 : rotX += 0.02;
+    top_side.rotation.z = rotZ;
+    top_side.position.set(rotX, rotY, 0)
+    console.log('open', rotX, rotY, rotZ);
+    front_side.rotation.x = - 2.3 * rotZ;
+    front_side.position.z = zDisplacementPositive;
+    front_side.position.y = yDisplacement;
+    
+    // front_side.position.y = 1.5;
+    back_side.rotation.x = 2.3 * rotZ;
+    back_side.position.z = zDisplacementNegative;
+    back_side.position.y = yDisplacement;
+
+    left_side.rotation.z = - 2.3 * rotZ;
+    left_side.position.x = xDisplacement;
+    left_side.position.y = yDisplacement;
+
+  } else {
+    clearInterval(intervalObj);
+  }
+}
+
+closeBox = () => {
+  if (rotZ <= 0) {
+    rotZ += 0.01;
+    rotY -= 0.048;
+    xDisplacement += 0.026;
+    yDisplacement += 0.018;
+    zDisplacementNegative += 0.024;
+    zDisplacementPositive -= 0.024;
+    (rotZ >= close_rot_z_val / 2) ? rotX -= 0.006 : rotX -= 0.013;
+    top_side.rotation.z = rotZ;
+    top_side.position.set(rotX, rotY, 0)
+    console.log(rotX, rotY, rotZ);
+
+    front_side.rotation.x = -2.3 * rotZ;
+    front_side.position.z = zDisplacementPositive;
+    front_side.position.y = yDisplacement;
+
+    back_side.rotation.x = 2.3 * rotZ;
+    back_side.position.z = zDisplacementNegative;
+    back_side.position.y = yDisplacement;
+
+    left_side.rotation.z = -2.3 * rotZ;
+    left_side.position.x = xDisplacement;
+    left_side.position.y = yDisplacement;
+  } else {
+    clearInterval(intervalObj);
+  }
+}
+
+selectView = side_index => {
+  scene.add(cube);
+  console.log('Side index ========>', side_index)
+  console.log('Side index ========>', typeof (side_index));
+  const box_sides = {
+    RIGHT: 0,
+    LEFT: 1,
+    TOP: 2,
+    BOTTOM: 3,
+    FRONT: 4,
+    BACK: 5
+  }
+  // cube.rotation.x = 0;
+  // cube.rotation.y = 0;
+  // cube.rotation.z = 0;
+
+  switch (parseInt(side_index)) {
+    case box_sides.RIGHT:
+      console.log('match===>', side_index);
+      cube.rotation.y = -Math.PI / 2;
+      top_side.rotation.y = -Math.PI / 2;
+      break;
+    case box_sides.LEFT:
+      console.log('match===>', side_index);
+      cube.rotation.y = Math.PI / 2;
+      top_side.rotation.y = Math.PI / 2;
+      break;
+    case box_sides.TOP:
+      console.log('match===>', side_index);
+      cube.rotation.x = Math.PI / 2;
+      top_side.rotation.x = Math.PI / 2;
+      break;
+    case box_sides.BOTTOM:
+      console.log('match===>', side_index);
+      cube.rotation.x = -Math.PI / 2;
+      top_side.rotation.x = -Math.PI / 2;
+      break;
+    case box_sides.FRONT:
+      console.log('match===>', side_index);
+      cube.rotation.x = 0;
+      // cube.rotation.y = Math.PI;
+      cube.rotation.y = 0;
+      cube.rotation.z = 0;
+      top_side.rotation.x = 0;
+      top_side.rotation.y = 0;
+      top_side.rotation.z = 0;
+      break;
+    case box_sides.BACK:
+      console.log('match===>', side_index);
+      cube.rotation.x = 0;
+      cube.rotation.y = Math.PI;
+      top_side.rotation.y = Math.PI;
+      cube.rotation.z = 0;
+      break;
+    default:
+      cube.rotation.x = 0;
+      cube.rotation.y = 0;
+      cube.rotation.z = 0;
+      break;
+      break;
+      break;
+  }
+
+  console.log('finish');
+
+}
+
+
+
+// const n1 = new THREE.Vector3( 0, 1, 0 );
+// const d1 = 2;
+// const q1 = new THREE.Vector3( 0, 2, 0 );
+// const plane1 = new THREE.Plane( n1, -d1 );
+// console.log( plane1.distanceToPoint( q1 ) );  
+// base.position = 10;
+
+
+
+
+// Draw Scene
+let render = () => {
+  renderer.render(scene, camera);
+}
+
+// run game loop(update, render, repeat)
+let GameLoop = () => {
+  requestAnimationFrame(GameLoop);
+
+  render();
+}
+//update();
+GameLoop()
+// </script>
