92f840cd4e2299228be9a442b129ab1d1f852c79
Sobhit Kumar <sobhit.kumar@claritusconsulting.com>
2019-10-21 11:41:58 +0530

    Adding right side flap

--- pivot2.js
+++ pivot2.js
@@ -23,7 +23,7 @@ window.addEventListener('resize', () => {
 // controls
 control = new THREE.OrbitControls(camera, renderer.domElement);
 const LENGTH = 10;
-const HEIGHT = 3;
+const HEIGHT = 2;
 const WIDTH = 8;
 const THICKNESS = 0.05;
 
@@ -31,7 +31,7 @@ const THICKNESS = 0.05;
 let bt_geometry = new THREE.BoxBufferGeometry(LENGTH, THICKNESS, WIDTH);
 let bt_material = new THREE.MeshBasicMaterial({ color: 0x0ff0ff, side: THREE.DoubleSide });
 let bottom = new THREE.Mesh(bt_geometry, bt_material);
-bottom.position.set(0, -1.5, 0);
+bottom.position.set(0, -1, 0);
 scene.add(bottom);
 camera.position.z = 10;
 
@@ -40,28 +40,31 @@ let rs_geometry = new THREE.BoxBufferGeometry(THICKNESS, HEIGHT, WIDTH);
 let rs_material = new THREE.MeshBasicMaterial({ color: 0x0bcdff, side: THREE.DoubleSide });
 let right_side = new THREE.Mesh(rs_geometry, rs_material);
 var rside = new THREE.Object3D();
-rside.rotation.z = -Math.PI / 4;
-right_side.position.y = 1.5;
+// rside.rotation.z = -Math.PI/2;
+right_side.position.y = 1;
 scene.add(rside);
 rside.add(right_side)
-// right_side.position.x = 5;
-// right_side.position.set(5, 0, 0);
 rside.position.x = 5;
-rside.position.y = -1.5;
-
-
+rside.position.y = -1;
 
 // Draw Scene
 let render = () => {
-  if(rside.rotation.z < -Math.PI/4){
-    rside.rotation.z -= 0.03;
-    console.log(rside.rotation.z);
-  }else if(rside.rotation.z < Math.PI/4){
-    rside.rotation.z += 0.03
-    console.log(rside.rotation.z);
-  }
-// right_side.position.y = 1.5;
 
+  if (rside.rotation.z >= -Math.PI / 2 && box_status==false) {
+    rside.rotation.z -= 0.02;
+    console.log('****************OPEN******************', rside.rotation.z, box_status);
+    if(rside.rotation.z<-Math.PI / 2) {
+      box_status = true
+    }
+  }
+
+ if (rside.rotation.z <= 0 && box_status==true) {
+    rside.rotation.z += 0.02;
+    console.log('*********************CLOSE****************',rside.rotation.z, box_status);
+    if(rside.rotation.z> 0) {
+      box_status = false
+    }
+  }
   renderer.render(scene, camera);
 }
 
